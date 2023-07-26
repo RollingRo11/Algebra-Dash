1f5a8c09270361f203b70a39605b0cb34d12116d
RollingRo11 <rohan.kathuria@live.com>
2023-07-26 16:39:24 -0500

    animation

--- src/scenes/TestLevel.js
+++ src/scenes/TestLevel.js
@@ -1,69 +1,115 @@
 export default class TestLevel extends Phaser.Scene{
     preload() {
-        this.load.image('background', './src/assets/clouds.png');
+        this.load.image('background', './src/assets/images/clouds.png');
+        this.load.atlas("player", './src/assets/spritesheets/Xsheet.png', './src/assets/spritesheets/Xsheet.json');
         //tileset this.load.image('tiles', 'tileset.png');
         //this.load.tilemapTiledJSON('map', '');
-        this.load.image('player', './src/assets/X.png')
     }
       
     create() {
-        //const map = this.make.tilemap({ key: 'map' });
-        //const tileset = map.addTilesetImage('tiles');
+
         const backgroundImage = this.add.image(0, 0, 'background').setOrigin(0, 0);
         backgroundImage.setScale(2, 1);
+
+        //TILESET STUFF (PUT IN AFTER TILESET/MAP IS IMPORTED)
+        //const map = this.make.tilemap({ key: 'map' });
+        //const tileset = map.addTilesetImage('tiles');
         //const platforms = map.createStaticLayer('Platforms', tileset, 0, 200);
         //platforms.setCollisionByExclusion(-1, true);
       
         this.player = this.physics.add.sprite(50, 300, 'player');
         this.player.setBounce(0.1);
         this.player.setCollideWorldBounds(true);
-       // this.physics.add.collider(this.player, platforms);
-      
-        /*
+        //this.physics.add.collider(this.player, platforms);
+        var isCurling;
+        var isRolling;
+
         this.anims.create({
-          key: 'walk',
-          frames: this.anims.generateFrameNames('player', {
-            prefix: 'variable-boi',
-            start: 2,
-            end: 3,
+          key: "curl",
+          frames: this.anims.generateFrameNames("player", {
+            prefix: "Curl",
+            end: 9,
+            zeroPad: 0,
           }),
-          frameRate: 10,
-          repeat: -1
+          repeat: 0,
+          framerate: 10,
         });
-      
+
         this.anims.create({
-          key: 'idle',
-          frames: [{ key: 'player', frame: 'variable_boi_0' }],
-          frameRate: 10,
+          key: "idle",
+          frames: this.anims.generateFrameNames("player", {
+            prefix: "Curl",
+            end: 0,
+            zeroPad: 0,
+          }),
+          repeat: 0,
         });
-      
+
         this.anims.create({
-          key: 'jump',
-          frames: [{ key: 'player', frame: 'variable_boi_1' }],
-          frameRate: 10,
+          key: "roll",
+          frames: this.anims.generateFrameNames("player", {
+            prefix: "Rolling",
+            end: 2,
+            zeroPad: 0,
+          }),
+          repeat: -1,
+          framerate: 20,
         });
-        */
+
+
+        this.anims.create({
+          key: "jump",
+          frames: this.anims.generateFrameNames("player", {
+            prefix: "Curl",
+            end: 0,
+            zeropad: 0,
+          }),
+          repeat: 0,
+        });
+
         this.cursors = this.input.keyboard.createCursorKeys();
+        this.keys = this.input.keyboard.addKeys({ up: 'W', left: 'A', down: 'S', right: 'D', space: 'SPACE' });
+      }
+
+      curlFinished() {
+        this.player.play('roll');
+        this.isCurling = false;
+        console.log('delayed')
+        this.isRolling = true;
+        
       }
       
     update() {
-        if (this.cursors.left.isDown) {
-          this.player.setVelocityX(-200);
-          if (this.player.body.onFloor()) {
-            this.player.play('walk', true);
-          }
-        } else if (this.cursors.right.isDown) {
+      
+        if ((this.keys.left.isDown) && (this.isCurling = false)) {
+          console.log('left key tapped')
+          if (this.player.body.onFloor()) {console.log('floored')}
+            this.player.play('curl')
+            this.isCurling = true;
+            console.log('curling')
+            this.time.delayedCall(1000, this.curlFinished, [], this);
+
+        } else if (this.keys.left.isDown){
+          this.player.setVelocityX(-200)
+          
+        } else if (this.keys.right.isDown) {
           this.player.setVelocityX(200);
+          console.log('right key tapped')
+          if (this.player.body.onFloor()) {console.log('floored')}
+            this.player.play('curl')
+            this.isCurling = true;
+            console.log('curling')
+            this.time.delayedCall(1000, this.curlFinished, [], this);
+
+        } else if (this.isCurling = false){
+          this.player.setVelocityX(0);
           if (this.player.body.onFloor()) {
-            this.player.play('walk', true);
-          }
+            this.player.play('idle', true);}
         } else {
           this.player.setVelocityX(0);
-          //if (this.player.body.onFloor()) {
-          //  this.player.play('idle', true);}
         }
       
-        if ((this.cursors.space.isDown || this.cursors.up.isDown) && this.player.body.onFloor()) {
+        if ((this.keys.space.isDown || this.keys.up.isDown) && this.player.body.onFloor()) {
           this.player.setVelocityY(-350);
           this.player.play('jump', true);
         }
