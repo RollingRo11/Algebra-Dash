c0ab5456b0c6a1d013cc529e0a0d053019bae0f3
RollingRo11 <rohan.kathuria@live.com>
2023-07-27 13:42:01 -0500

    test demo

--- src/scenes/TestLevel.js
+++ src/scenes/TestLevel.js
@@ -3,31 +3,46 @@ export default class TestLevel extends Phaser.Scene{
         this.load.image('background', './src/assets/images/skyBackground.png');
         this.load.atlas("player", './src/assets/spritesheets/Xsheet.png', './src/assets/spritesheets/Xsheet.json');
         this.load.image('tiles', './src/assets/tilesets/tileset.png');
-        this.load.image('gun', './src/assets/images/gun.png');
+        this.load.image('gun', './src/assets/images/SQRT.png');
         this.load.image('middle', './src/assets/images/middle.png');
         // Load the export Tiled JSON
         this.load.tilemapTiledJSON('map', './src/assets/tilemaps/levelOne.json');
+        
     }
       
     create() {
+      this.background = this.add.tileSprite(0, 0, 64000, 2240, "background")
+      .setOrigin(0)
+      .setScrollFactor(0, 1);
+
 
         //const backgroundImage = this.add.image(0, 0, 'background').setOrigin(0, 0);
         //backgroundImage.setScale(2, 1);
 
         //TILESET STUFF (PUT IN AFTER TILESET/MAP IS IMPORTED)
+        const arrayRange = (start, stop, step) =>
+        Array.from({ length: (stop - start) / step + 1 },
+        (value, index) => start + index * step
+        );
+
+console.log(arrayRange(1, 5, 1)); // [1,2,3,4,5]
         const map = this.make.tilemap({ key: 'map',});
         const tileset = map.addTilesetImage('tileset', 'tiles');
-        const platform = map.createLayer('Ground Layer', tileset, 0, 0);
+        
+        this.platform = map.createLayer('Ground Layer', tileset, 0, 0);
+        this.platform.setCollisionByProperty({ collides: true });
+
         const EnemyLayer = map.createLayer('Enemy Layer', tileset, 0, 0);
         const Water = map.createLayer('Extra Water', tileset, 0, 0);
         const props = map.createLayer('Prop Layer', tileset, 0, 0);
         const Fence = map.createLayer('Fence', tileset, 0, 0);
         const Crate = map.createLayer('Crate', tileset, 0, 0);
+        
         var camera;
 
-        this.cameras.main.setBounds(0, 0, 3840, 1080, true);
-        this.physics.world.setBounds(0, 0, 64000, 2240);
-
+        this.cameras.main.setBounds(0, -700, 64000, 100000, true);
+        this.physics.world.setBounds(0, -700, 64000, 100000);
+        map.setCollision(arrayRange(1, 10000, 1), true, false, 'Ground Layer', true);
 
 
 
@@ -36,14 +51,11 @@ export default class TestLevel extends Phaser.Scene{
         this.player = this.physics.add.sprite(0, 0, 'player');
         this.player.setBounce(0.1);
         this.player.setCollideWorldBounds(true);
-        this.physics.add.collider(this.player, platform);   
-        map.setCollisionBetween(1, 999, true, 'Platform');   
+        this.physics.add.collider(this.player, this.platform);   
+        map.setCollisionBetween(1, 999, true, this.platform);   
         this.cameras.main.startFollow(this.player, true);
 
-        this.gun = this.physics.add.sprite(0, 0, 'gun');
-        this.gun.setBounce(0.1);
-        this.gun.setCollideWorldBounds(true);
-        this.physics.add.collider(this.gun, platform);
+        this.gun = this.add.sprite(0, 0, 'gun');
 
         this.anims.create({
           key: "curl",
@@ -113,26 +125,26 @@ export default class TestLevel extends Phaser.Scene{
 
         } else if (this.keys.left.isDown){
           this.player.setVelocityX(-500);
-          this.gun.setVelocityX(-500);
+          //this.gun.setVelocityX(-500);
         
         } else if (Phaser.Input.Keyboard.JustDown(this.keys.right)){
             this.player.play('curl')
             this.time.delayedCall(300, this.curlFinished, [], this);
           } else if (this.keys.right.isDown){
             this.player.setVelocityX(500);
-            this.gun.setVelocityX(500);
+            //this.gun.setVelocityX(500);
 
 
         } else if (Phaser.Input.Keyboard.JustUp(this.keys.right) || Phaser.Input.Keyboard.JustUp(this.keys.left)) {
             this.player.playReverse('curl');
             this.time.delayedCall(300, this.recurlFinished, [], this);
           this.player.setVelocityX(0);
-          this.gun.setVelocityX(0);
+          //this.gun.setVelocityX(0);
         }
       
         if ((this.keys.space.isDown || this.keys.up.isDown) && this.player.body.onFloor()) {
-          this.player.setVelocityY(-600);
-          this.gun.setVelocityY(-600);
+          this.player.setVelocityY(-400);
+          //this.gun.setVelocityY(-600);
         }
       
         if (this.player.body.velocity.x > 0) {
@@ -141,9 +153,11 @@ export default class TestLevel extends Phaser.Scene{
         } else if (this.player.body.velocity.x < 0) {
           this.player.setFlipX(true);
           this.gun.setFlipX(true);
-
         }
+        this.gun.x = this.player.x;
+        this.gun.y = this.player.y;
       }
+
 }
 
 
