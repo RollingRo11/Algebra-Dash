4c95bbfb9ba4a5226a3dafb96047bea93b80e5a7
RollingRo11 <rohan.kathuria@live.com>
2023-07-27 08:27:02 -0500

    animations finally work. Test tilemap. Gun physics in the works

--- src/scenes/TestLevel.js
+++ src/scenes/TestLevel.js
@@ -2,8 +2,9 @@ export default class TestLevel extends Phaser.Scene{
     preload() {
         this.load.image('background', './src/assets/images/clouds.png');
         this.load.atlas("player", './src/assets/spritesheets/Xsheet.png', './src/assets/spritesheets/Xsheet.json');
-        //this.load.image('tilesheet', './src/assets/tilesets/tileset.png');
-        //this.load.tilemapTiledJSON('tilemap', './src/assets/tilemaps/Level1AlgebraDash.json');
+        this.load.image('tiles', './src/assets/tilesets/tileset.png');
+    // Load the export Tiled JSON
+    this.load.tilemapTiledJSON('map', './src/assets/tilemaps/test4.json');
     }
       
     create() {
@@ -12,26 +13,32 @@ export default class TestLevel extends Phaser.Scene{
         backgroundImage.setScale(2, 1);
 
         //TILESET STUFF (PUT IN AFTER TILESET/MAP IS IMPORTED)
-        /*
-        const map = this.make.tilemap({ key: 'tilemap' });
-        const tileset = map.addTilesetImage('levelOneTiles', 'tilesheet');
-        map.createStaticLayer('Image Layer 1', tileset)
-        map.createStaticLayer('Imagxe Layer 2', tileset)
-        map.createStaticLayer('Tile Layer 6', tileset)
-        map.createStaticLayer('Tile Layer 5', tileset)
-        map.createStaticLayer('Prop Tile', tileset)
-        map.createStaticLayer('Enemy Markers', tileset)
-        map.createStaticLayer('Tile Layer 2', tileset)
-        map.createStaticLayer('Ground Layer', tileset)
-        //platforms.setCollisionByExclusion(-1, true);
-        */
-      
-        this.player = this.physics.add.sprite(50, 300, 'player');
+        const map = this.make.tilemap({ key: 'map' });
+        const tileset = map.addTilesetImage('tileset', 'tiles');
+        const platform = map.createLayer('Tile Layer 1', tileset, 0, 0);
+        var camera;
+
+        this.cameras.main.setBounds(0, 0, 3840, 1080, true);
+        this.physics.world.setBounds(0, 0, 64000, 2240);
+
+
+
+
+        //PLAYER PACKAGE:----------------------------------------------------------------
+        
+        this.player = this.physics.add.sprite(0, 0, 'player');
+        this.gun = this.add.sprite(0, 0, 'gun');
+        const playerPackage = this.add.container(0, 0, ['player', 'gun']);
         this.player.setBounce(0.1);
         this.player.setCollideWorldBounds(true);
-        //this.physics.add.collider(this.player, platforms);
-        var isCurling;
-        var isRolling;
+        this.physics.add.collider(this.player, platform);   
+        map.setCollisionBetween(1, 999, true, 'Platform');   
+
+        //this.cameras.main.setSize(1920, 1080);
+        this.cameras.main.startFollow(this.player, true);
+
+
+        
 
         this.anims.create({
           key: "curl",
@@ -41,7 +48,18 @@ export default class TestLevel extends Phaser.Scene{
             zeroPad: 0,
           }),
           repeat: 0,
-          framerate: 10,
+          framerate: 20,
+        });
+
+        this.anims.create({
+          key: "recurl",
+          frames: this.anims.generateFrameNames("player", {
+            prefix: "Curl",
+            end: 0,
+            zeroPad: 0,
+          }),
+          frameRate: 60,
+          repeat: -0,
         });
 
         this.anims.create({
@@ -62,65 +80,45 @@ export default class TestLevel extends Phaser.Scene{
             zeroPad: 0,
           }),
           repeat: -1,
-          framerate: 20,
+          framerate: 80,
         });
-
-
-        this.anims.create({
-          key: "jump",
-          frames: this.anims.generateFrameNames("player", {
-            prefix: "Curl",
-            end: 0,
-            zeropad: 0,
-          }),
-          repeat: 0,
-        });
-
+        
+        //generate keys for user input
         this.cursors = this.input.keyboard.createCursorKeys();
         this.keys = this.input.keyboard.addKeys({ up: 'W', left: 'A', down: 'S', right: 'D', space: 'SPACE' });
       }
 
       curlFinished() {
-        this.player.play('roll');
-        this.isCurling = false;
-        console.log('delayed')
-        this.isRolling = true;
-        
+        this.player.play('roll', true);
+      }
+
+      recurlFinished() {
+        this.player.play('idle', true);
       }
       
     update() {
-      this.isCurling = false;
-      
-        if ((this.keys.left.isDown) && (this.isCurling = false)) {
-          console.log('left key tapped')
-          if (this.player.body.onFloor()) {console.log('floored')}
+        if (Phaser.Input.Keyboard.JustDown(this.keys.left)){          
             this.player.play('curl')
-            this.isCurling = true;
-            console.log('curling')
-            this.time.delayedCall(1000, this.curlFinished, [], this);
+            this.time.delayedCall(300, this.curlFinished, [], this);
 
         } else if (this.keys.left.isDown){
-          this.player.setVelocityX(-200)
-          
-        } else if ((this.keys.right.isDown) && (this.isCurling)) {
-          console.log('right key tapped')
-          if (this.player.body.onFloor()) {console.log('floored')}
+          this.player.setVelocityX(-500);
+        
+        } else if (Phaser.Input.Keyboard.JustDown(this.keys.right)){
             this.player.play('curl')
-            this.isCurling = true;
-            console.log('curling')
-            this.time.delayedCall(1000, this.curlFinished, [], this);
+            this.time.delayedCall(300, this.curlFinished, [], this);
+          } else if (this.keys.right.isDown){
+            this.player.setVelocityX(500);
 
-        } else if (this.isCurling = false){
+        } else if (Phaser.Input.Keyboard.JustUp(this.keys.right) || Phaser.Input.Keyboard.JustUp(this.keys.left)) {
+            this.player.playReverse('curl');
+            this.time.delayedCall(300, this.recurlFinished, [], this);
           this.player.setVelocityX(0);
-          if (this.player.body.onFloor()) {
-            this.player.play('idle', true);}
-        } else {
-          this.player.setVelocityX(0);
+
         }
       
         if ((this.keys.space.isDown || this.keys.up.isDown) && this.player.body.onFloor()) {
-          this.player.setVelocityY(-350);
-          this.player.play('jump', true);
+          this.player.setVelocityY(-600);
         }
       
         if (this.player.body.velocity.x > 0) {
